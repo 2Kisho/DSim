ea1a589a9158f093c68767cee2254f5f2f6ed719
Bastian Lange <bastian_lange@hotmail.com>
2020-09-26 09:03:30 +0200

    Added Javadoc for the UtilLogger

--- DSim/src/main/java/de/nerdfactory/dsim/util/UtilLogger.java
+++ DSim/src/main/java/de/nerdfactory/dsim/util/UtilLogger.java
@@ -3,8 +3,22 @@ package de.nerdfactory.dsim.util;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+/**
+ * The {@link UtilLogger} provides a method {@link #getStacktrace(Throwable)} to get a Stacktrace
+ * from a {@link Throwable}.
+ * 
+ * @author basti
+ *
+ */
 public class UtilLogger {
 
+    /**
+     * Gets a stacktrace from a given {@link Throwable}.
+     * 
+     * @param t
+     *            A {@link Throwable}
+     * @return A String with the stacktrace from the {@link Throwable}.
+     */
     public static String getStacktrace(Throwable t) {
         StringWriter sw = new StringWriter(512);
         PrintWriter pw = new PrintWriter(sw);
