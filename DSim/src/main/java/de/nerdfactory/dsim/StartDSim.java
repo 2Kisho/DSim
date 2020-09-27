1d272f28f849ad74208dae7232c4a15017cca6f0
Bastian Lange <bastian_lange@hotmail.com>
2020-09-27 07:12:05 +0200

    Added more possibility to the UtilRes with javadoc.

--- DSim/src/main/java/de/nerdfactory/dsim/StartDSim.java
+++ DSim/src/main/java/de/nerdfactory/dsim/StartDSim.java
@@ -7,5 +7,7 @@ public class StartDSim {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(UtilRes.getString("test"));
+        System.out.println(UtilRes.getString(StartDSim.class, "test"));
+        System.out.println(UtilRes.getString(Class.class, "notExisting"));
     }
 }
--- DSim/src/main/java/de/nerdfactory/dsim/util/UtilRes.java
+++ DSim/src/main/java/de/nerdfactory/dsim/util/UtilRes.java
@@ -1,13 +1,57 @@
 package de.nerdfactory.dsim.util;
 
+import java.util.MissingResourceException;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
+/**
+ * The {@link UtilRes} holds the {@link ResourceBundle} for the application.
+ * 
+ * @author basti
+ *
+ */
 public class UtilRes {
 
-	private static final String BASE_NAME = "DSim";
-	
-	public static String getString(String key) {
-		ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME);
-		return bundle.getString(key); 
-	}
+    private static final Logger LOGGER = Logger.getLogger(UtilRes.class.getName());
+    private static final String BASE_NAME = "DSim";
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BASE_NAME);
+
+    private UtilRes() {
+        throw new IllegalStateException("No instance for you!");
+    }
+
+    /**
+     * Retrieves the translation from the {@link ResourceBundle} wit the given key in the manner of:
+     * 
+     * <pre>
+     * ClazzName.key
+     * </pre>
+     * 
+     * @param clazz
+     *            The {@link Class} thats name should be used.
+     * @param key
+     *            A {@link String} that indicates the key.
+     * @return A String with the translation, if no key was found the key itself.
+     */
+    public static String getString(Class<?> clazz, String key) {
+        return getString(clazz.getSimpleName() + "." + key);
+    }
+
+    /**
+     * Retrieves the translation from the {@link ResourceBundle} with the given key.
+     * 
+     * @param key
+     *            A String that determines which translation should be used.
+     * @return A String with the translation, if no key was found the key itself.
+     */
+    public static String getString(String key) {
+        Objects.requireNonNull(key, "The key must not be null!");
+        try {
+            return BUNDLE.getString(key);
+        } catch (MissingResourceException ex) {
+            LOGGER.fine("The key(" + key + ") was not found in the ResourceBundle!");
+            return key;
+        }
+    }
 }
