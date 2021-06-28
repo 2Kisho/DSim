f7ff5f57db240849439ffbcf314267aa07e0a40e
Bastian Lange <bastian_lange@hotmail.com>
2021-06-28 18:58:46 +0200

    Deprecated the DSimPanel, to remove it in the future.

--- DSim/src/main/java/de/nerdfactory/dsim/ui/DSimPanel.java
+++ DSim/src/main/java/de/nerdfactory/dsim/ui/DSimPanel.java
@@ -4,6 +4,12 @@ import java.util.Optional;
 
 import javax.swing.JPanel;
 
+/**
+ * Please use the {@link Tab} interface.
+ * @author basti
+ *
+ */
+@Deprecated
 public interface DSimPanel {
 
 	Optional<String> getTitle();
