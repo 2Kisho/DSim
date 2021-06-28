131af824460af3fa025a36ee17794008759293c1
Bastian Lange <bastian_lange@hotmail.com>
2021-06-28 19:08:03 +0200

    Cleaned up the starting application

--- DSim/src/main/java/de/nerdfactory/dsim/StartDSim.java
+++ DSim/src/main/java/de/nerdfactory/dsim/StartDSim.java
@@ -1,21 +1,10 @@
 package de.nerdfactory.dsim;
 
-import de.nerdfactory.dsim.rkub.RummikubGame;
+import de.nerdfactory.dsim.ui.DSimFrame;
 
 public class StartDSim {
 
     public static void main(String[] args) {
-//        List<Card> cards = new CardDeckBuilderImpl().setThirtyTwo(true).build();
-//        cards.forEach(e -> System.out.println(e));
-//        System.out.println(cards.size());
-//        DSimFrame frame = new DSimFrame();
-//        SkatPanelModel model = new SkatPanelModel();
-//        SkatPanelView view = new SkatPanelView();
-//        SkatPanelPresenter presenter = new SkatPanelPresenter(model, view);
-//        presenter.present();
-//        frame.setMainPanel(presenter);
-    	RummikubGame game = new RummikubGame(2);
-    	game.getPlayers().stream().forEach(e -> System.out.println(e));
-    	System.out.println(game);
+    	new DSimFrame();
     }
 }
