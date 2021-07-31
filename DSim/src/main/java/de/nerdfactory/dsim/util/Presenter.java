2b35d86139f2ec1939cb33437e257a5d3fdeb21b
Bastian Lange <bastian_lange@hotmail.com>
2021-07-31 14:28:11 +0200

    Added javadoc to basic interface presenter

--- DSim/src/main/java/de/nerdfactory/dsim/util/Presenter.java
+++ DSim/src/main/java/de/nerdfactory/dsim/util/Presenter.java
@@ -1,6 +1,15 @@
 package de.nerdfactory.dsim.util;
 
+/**
+ * This is a marker interface for the {@link Presenter} pattern.
+ * 
+ * @author basti
+ *
+ */
 public interface Presenter {
 
+	/**
+	 * Wiring up everything and presents the whole View.
+	 */
 	void present();
 }
